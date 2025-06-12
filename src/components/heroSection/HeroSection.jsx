6b524941f124c681063e8a7e1b6cbdf1755b5cd3
mithcodes <singhmithlesh791@gmail.com>
2025-06-13 01:25:33 +0530

    project setup and all files created

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,13 @@
+const HeroSection = () => {
+  return (
+    <div className="w-full h-[60vh] md:h-[60vh]">
+      <img
+        className="w-full h-full   object-cover"
+        src="https://t4.ftcdn.net/jpg/02/49/50/15/240_F_249501541_XmWdfAfUbWAvGxBwAM0ba2aYT36ntlpH.jpg"
+        alt="Hero Image"
+      />
+    </div>
+  );
+};
+
+export default HeroSection;
