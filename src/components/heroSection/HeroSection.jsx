d9dea8794d0c9e4bdf53fbd4282e1b9ad36eaddc
mithcodes <singhmithlesh791@gmail.com>
2025-07-28 00:53:10 +0530

    new features added

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,9 +1,25 @@
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
+import { X } from "lucide-react";
 
 const HeroSection = () => {
+
+   const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const isPopupClosed = localStorage.getItem("adminPopupClosed");
+    if (!isPopupClosed) {
+      setShowPopup(true);
+    }
+  }, []);
+
+const handleClose = () => {
+    setShowPopup(false);
+    localStorage.setItem("adminPopupClosed", "true");
+  };
+
   const settings = {
     dots: true,              // dots below slider
     infinite: true,          // infinite scroll
@@ -17,6 +33,27 @@ const HeroSection = () => {
 
 
 <div className="w-full  mx-auto mt-22 md:mt-22 overflow-x-hidden overflow-y-hidden ">
+      
+      
+      {showPopup && (
+        <div className="absolute top-26 left-1/2 -translate-x-1/2 bg-yellow-50 border border-yellow-300 text-yellow-800 px-6 py-4 rounded-lg shadow-md z-20 flex items-start justify-between gap-4 w-[90%] md:max-w-xl animate-fade-in">
+          <div className="text-sm md:text-base font-medium">
+            <p>
+              Please login as admin with email to see admin dashboard{" "}
+              <strong>admin@gmail.com</strong> and password{" "}
+              <strong>admin@gmail.com</strong>
+            </p>
+          </div>
+          <button
+            onClick={handleClose}
+            className="text-yellow-800 font-bold text-xl hover:text-red-600"
+          >
+            &times;
+          </button>
+        </div>
+      )}
+
+      
       <Slider {...settings}>
         <div>
           <img src="/image/hero8.jpg" alt="Slide 1" className="w-full h-[50vh] md:h-[70vh] object-cover" />
